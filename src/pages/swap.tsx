7edabaacaa00d37de4ca501c50e9830184400ee6
thal0x <thal0x.eth@gmail.com>
2023-06-29 09:01:41 -0500

    handle no routes

--- src/pages/swap.tsx
+++ src/pages/swap.tsx
@@ -361,9 +361,40 @@ function SwapPage() {
                 ]}
                 chainIDs={chainIDs}
               />
-              <div className="bg-indigo-400/10 border border-indigo-400/30 text-indigo-400 text-sm font-medium text-center rounded py-2">
-                <p>This route can be completed in a single transaction</p>
-              </div>
+              {!swapRouteResponse && isInitialLoading && (
+                <div className="bg-indigo-400/10 border border-indigo-400/30 text-indigo-400 text-sm font-medium text-center rounded py-2">
+                  <svg
+                    className="animate-spin -ml-1 mr-3 h-4 w-4 inline-block text-indigo-500"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                </div>
+              )}
+              {!swapRouteResponse && !isInitialLoading && (
+                <div className="bg-red-400/10 border border-red-400/30 text-red-400 text-sm font-medium text-center rounded py-2">
+                  <p>No route found</p>
+                </div>
+              )}
+              {swapRouteResponse && (
+                <div className="bg-indigo-400/10 border border-indigo-400/30 text-indigo-400 text-sm font-medium text-center rounded py-2">
+                  <p>This route can be completed in a single transaction</p>
+                </div>
+              )}
               {swapRouteResponse && (
                 <div>
                   <Accordion.Root className="AccordionRoot" type="multiple">
