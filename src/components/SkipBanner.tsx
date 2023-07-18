b2a39ecec7eebdcb081f450af48e50764be8bc5b
thal0x <thal0x.eth@gmail.com>
2023-07-18 23:04:09 +0200

    agrobanner

--- src/components/SkipBanner.tsx
+++ src/components/SkipBanner.tsx
@@ -3,29 +3,125 @@ import { FC } from "react";
 const SkipBanner: FC = () => {
   return (
     <div className="bg-[#FF486E] font-semibold text-sm text-white py-2">
-      <div className="flex items-center justify-between gap-2 px-6 w-[400px]">
-        <p>Powered by the Skip API </p>
-        <a
-          className="bg-indigo-400 px-2 py-1 pr-1 rounded inline-flex items-center gap-1 hover:underline"
-          href="https://skip.money/docs/frontends"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <span>Learn More</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-5 h-5"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </a>
-      </div>
+      {/* animate-banner-rotate */}
+      <a
+        href="https://skip.money/docs/frontends"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div className="overflow-hidden">
+          <div className="w-[5000px] flex items-center animate-banner-rotate">
+            <div className="w-[1000px] inline-flex items-center justify-around">
+              <span>Powered by the Skip API</span>
+              <div className="bg-indigo-400 px-2 py-1 pr-1 rounded inline-flex items-center gap-1">
+                <span>Learn More</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="w-[1000px] inline-flex items-center justify-around">
+              <span>Powered by the Skip API</span>
+              <div className="bg-indigo-400 px-2 py-1 pr-1 rounded inline-flex items-center gap-1">
+                <span>Learn More</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="w-[1000px] inline-flex items-center justify-around">
+              <span>Powered by the Skip API</span>
+              <div className="bg-indigo-400 px-2 py-1 pr-1 rounded inline-flex items-center gap-1">
+                <span>Learn More</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="w-[1000px] inline-flex items-center justify-around">
+              <span>Powered by the Skip API</span>
+              <div className="bg-indigo-400 px-2 py-1 pr-1 rounded inline-flex items-center gap-1">
+                <span>Learn More</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="w-[1000px] inline-flex items-center justify-around">
+              <span>Powered by the Skip API</span>
+              <div className="bg-indigo-400 px-2 py-1 pr-1 rounded inline-flex items-center gap-1">
+                <span>Learn More</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="w-[1000px] inline-flex items-center justify-around">
+              <span>Powered by the Skip API</span>
+              <div className="bg-indigo-400 px-2 py-1 pr-1 rounded inline-flex items-center gap-1">
+                <span>Learn More</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
     </div>
   );
 };
