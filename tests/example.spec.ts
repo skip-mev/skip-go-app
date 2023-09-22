5e9e798f4006b7f50667f0424265b49669077fd9
thal0x <thal0x.eth@gmail.com>
2023-09-22 08:26:34 -0500

    re-enable fixtures

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,4 @@
 import { expect, test } from "./lib/fixtures";
-// import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto("https://playwright.dev/");
