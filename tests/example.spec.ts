3b22ee436d176e7090e2753eb9dd1e5e4007d20f
thal0x <thal0x.eth@gmail.com>
2023-09-22 08:15:01 -0500

    test without fixtures

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,5 @@
-import { expect, test } from "./lib/fixtures";
+// import { expect, test } from "./lib/fixtures";
+import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto("https://playwright.dev/");
