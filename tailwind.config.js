c9a46b522cd9a9d3271abc61e45eaecadeff05f8
Griko Nibras <griko@nibras.co>
2023-10-26 22:25:22 +0700

    feat: add tailwind defs for radix accordion

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,16 +21,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        "accordion-open": `accordion-open 150ms cubic-bezier(0.87, 0, 0.13, 1)`,
+        "accordion-closed": `accordion-closed 150ms cubic-bezier(0.87, 0, 0.13, 1)`,
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": `conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))`,
         "site-bg": "url('/site-bg.svg')",
         "site-bg-2": "url('/site-bg-2.svg')",
         squiggle: "url('/squiggle.svg')",
       },
       fontFamily: {
         sans: ["Jost", "sans-serif"],
+      },
+      keyframes: {
+        "accordion-open": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-closed": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
       },
     },
   },
