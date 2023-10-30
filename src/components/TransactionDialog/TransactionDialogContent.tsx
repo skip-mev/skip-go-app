7a09298fa8000e283e570c155660717c7f71184d
Griko Nibras <griko@nibras.co>
2023-10-31 05:06:55 +0700

    chore: TxDialogContent style housekeeping

--- src/components/TransactionDialog/TransactionDialogContent.tsx
+++ src/components/TransactionDialog/TransactionDialogContent.tsx
@@ -1,6 +1,7 @@
 import { useManager } from "@cosmos-kit/react";
 import { ArrowLeftIcon, CheckCircleIcon } from "@heroicons/react/20/solid";
 import { RouteResponse } from "@skip-router/core";
+import { clsx } from "clsx";
 import { FC, Fragment, useState } from "react";
 import { useAccount } from "wagmi";
 
@@ -299,7 +300,12 @@ const TransactionDialogContent: FC<Props> = ({
         </div>
         <div className="space-y-4">
           <button
-            className="bg-[#FF486E] text-white font-semibold py-4 rounded-md w-full transition-transform enabled:hover:scale-105 enabled:hover:rotate-1 disabled:cursor-not-allowed disabled:opacity-75 outline-none"
+            className={clsx(
+              "bg-[#FF486E] text-white font-semibold py-4 rounded-md w-full",
+              "transition-transform outline-none",
+              "enabled:hover:scale-105 enabled:hover:rotate-1",
+              "disabled:cursor-not-allowed disabled:opacity-75",
+            )}
             onClick={onSubmit}
             disabled={transacting || insufficentBalance}
           >
@@ -312,17 +318,17 @@ const TransactionDialogContent: FC<Props> = ({
               >
                 <circle
                   className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
+                  cx={12}
+                  cy={12}
+                  r={10}
                   stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
+                  strokeWidth={4}
+                />
                 <path
                   className="opacity-75"
                   fill="currentColor"
                   d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
+                />
               </svg>
             ) : (
               <span>Submit</span>
