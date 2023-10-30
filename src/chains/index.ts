9f541aeed906160adfcfafe3f50d092cad0b03e3
Griko Nibras <griko@nibras.co>
2023-10-31 01:40:52 +0700

    refactor: rewrite codegen from scratch

--- src/chains/index.ts
+++ src/chains/index.ts
@@ -1,24 +1,43 @@
 /* eslint-disable */
 // @ts-nocheck
-import type { Asset, Chain } from "@chain-registry/types";
+import { Asset, AssetList, Chain } from "@graz-sh/types";
 
-import { raise } from "@/utils/assert";
+import {
+  assetsRecord,
+  ChainIdOrName,
+  chainIds,
+  chainIdToName,
+  chainNameToId,
+  chainRecord,
+} from "./generated";
 
-import type { ChainIdOrName } from "./generated";
-import { assetsRecord, chainNameToId, chainRecord } from "./generated";
+function raise(message?: string, opts?: ErrorOptions): never {
+  throw new Error(message, opts);
+}
 
-export const getChain = (idOrName: ChainIdOrName): Chain => {
+export function getChain(idOrName: ChainIdOrName): Chain {
   return (
     chainRecord[chainNameToId[idOrName]] ||
     raise(`chain '${idOrName}' does not exist in chainRecord`)
   );
-};
+}
 
-export const getAssets = (idOrName: ChainIdOrName): Asset[] => {
+export function getAssets(idOrName: ChainIdOrName): Asset[] {
   return (
     assetsRecord[chainNameToId[idOrName]] ||
     raise(`chain '${idOrName}' does not exist in assetsRecord`)
   );
-};
+}
+
+export function getChains(): Chain[] {
+  return Object.values(chainRecord);
+}
+
+export function getAssetLists(): AssetList[] {
+  return chainIds.map((chainId) => ({
+    chain_name: chainIdToName[chainId],
+    assets: assetsRecord[chainId],
+  }));
+}
 
 export * from "./generated";
