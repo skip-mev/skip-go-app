f911879046e40cc5041976c0bf82e3ad2ab503c9
Griko Nibras <griko@nibras.co>
2023-11-02 01:43:01 +0700

    feat: init settings dialog

---
+++ src/components/SettingsDialog/index.tsx
@@ -0,0 +1,26 @@
+import { ArrowLeftIcon } from "@heroicons/react/20/solid";
+
+import { useDisclosureKey } from "@/context/disclosures";
+
+export const SettingsDialog = () => {
+  const [isOpen, { close }] = useDisclosureKey("settingsDialog");
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="absolute inset-0 bg-white rounded-3xl z-[999]">
+      <div className="h-full px-4 py-6 overflow-y-auto scrollbar-hide">
+        <div className="flex items-center gap-4 pb-2">
+          <button
+            className="hover:bg-neutral-100 w-8 h-8 rounded-full flex items-center justify-center transition-colors"
+            onClick={close}
+          >
+            <ArrowLeftIcon className="w-6 h-6" />
+          </button>
+          <h3 className="font-bold text-xl">Swap Settings</h3>
+        </div>
+        {/* TODO */}
+      </div>
+    </div>
+  );
+};
