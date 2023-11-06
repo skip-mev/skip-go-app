a9d2ea30cee894c9d62a81946fd85d48108c9f5f
Griko Nibras <griko@nibras.co>
2023-11-07 03:57:27 +0700

    feat: configure sentry in next.config.js
    config pattern follows https://github.com/skip-mev/dydx-dashboard/blob/staging/next.config.js
    
    Signed-off-by: Griko Nibras <griko@nibras.co>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-const nextConfig = {
+/**
+ * @type {import('next').NextConfig}
+ * @see https://nextjs.org/docs/pages/api-reference/next-config-js
+ */
+let nextConfig = {
   productionBrowserSourceMaps: true,
   rewrites: async () => {
     return [
@@ -35,5 +39,33 @@ const nextConfig = {
         ]
       : [],
 };
+
+/** @see https://docs.sentry.io/platforms/javascript/guides/nextjs */
+const { withSentryConfig } = require("@sentry/nextjs");
+
+/**
+ * @type {Partial<import('@sentry/nextjs').SentryWebpackPluginOptions>}
+ * @see https://github.com/getsentry/sentry-webpack-plugin#options
+ */
+const sentryWebpackConfig = {
+  org: "skip-protocol",
+  project: "ibc-dot-fun",
+  silent: true,
+};
+
+/**
+ * @type {import('@sentry/nextjs/types/config/types').UserSentryOptions}
+ * @see https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup
+ */
+const sentryOptions = {
+  disableLogger: true,
+  hideSourceMaps: false,
+  transpileClientSDK: true,
+  tunnelRoute: "/monitoring",
+  widenClientFileUpload: true,
+};
+
+/** @see https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup */
+nextConfig = withSentryConfig(nextConfig, sentryWebpackConfig, sentryOptions);
 
 module.exports = nextConfig;
