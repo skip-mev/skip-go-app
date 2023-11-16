5cb50dc4a52fd4de36030adc0e589d2bc1f88414
Griko Nibras <griko@nibras.co>
2023-11-17 03:47:57 +0700

    feat: create useUsdDiffValue hook
    Signed-off-by: Griko Nibras <griko@nibras.co>

--- src/hooks/useUsdValue.ts
+++ src/hooks/useUsdValue.ts
@@ -32,16 +32,46 @@ export function useUsdValue({ chainId, denom, value }: Args) {
   });
 }
 
+export function useUsdDiffValue([args1, args2]: [Args, Args]) {
+  const queryKey = useMemo(() => {
+    const hash = [...Object.values(args1), ...Object.values(args2)].join("-");
+    return ["USE_USD_DIFF_VALUES", hash] as const;
+  }, [args1, args2]);
+
+  const enabled = useMemo(() => {
+    const parsed1 = parseFloat(args1.value);
+    const parsed2 = parseFloat(args2.value);
+    return !isNaN(parsed1) && parsed1 > 0 && !isNaN(parsed2) && parsed2 > 0;
+  }, [args1.value, args2.value]);
+
+  return useQuery({
+    // intentionally not including args1 and args2 since query key is using
+    // hashed values of args1 and args2
+    // eslint-disable-next-line @tanstack/query/exhaustive-deps
+    queryKey,
+    queryFn: async () => {
+      const [v1, v2] = await Promise.all([
+        getUsdValue(args1),
+        getUsdValue(args2),
+      ]);
+      // return percentage difference
+      return ((v2 - v1) / v1) * 100;
+    },
+    staleTime: 1000 * 60, // 1 minute
+    enabled,
+  });
+}
+
 async function getUsdValue({ chainId, denom, value }: Args) {
-      const assets = getAssets(chainId);
-      const asset =
-        assets.find((asset) => asset.base === denom) ||
+  const assets = getAssets(chainId);
+  const asset =
+    assets.find((asset) => asset.base === denom) ||
     raise(`getUsdValue error: ${denom} not found in ${chainId}`);
-      const coingeckoId =
-        asset.coingecko_id ||
-        raise(
+  const coingeckoId =
+    asset.coingecko_id ||
+    raise(
       `getUsdValue error: ${denom} does not have a 'coingecko_id' in ${chainId}`,
-        );
-      const usd = await getUsdPrice({ coingeckoId });
-      return parseFloat(value) * usd;
+    );
+  const usd = await getUsdPrice({ coingeckoId });
+  return parseFloat(value) * usd;
 }
