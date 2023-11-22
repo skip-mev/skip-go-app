9f42bedb9e3b7a34f147825c15588236f00b0b2c
Griko Nibras <griko@nibras.co>
2023-11-23 03:32:56 +0700

    refactor: rework usd diff internal context

--- src/components/UsdValue.tsx
+++ src/components/UsdValue.tsx
@@ -1,10 +1,5 @@
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import { createContext, ReactNode, useContext, useEffect, useRef } from "react";
+import { create } from "zustand";
 
 import { Args, useUsdDiffValue, useUsdValue } from "@/hooks/useUsdValue";
 
@@ -20,71 +15,77 @@ export const UsdValue = ({
 }: UsdValueProps) => {
   const { data: usdValue = 0, isLoading } = useUsdValue(args);
 
-  const ctx = useContext(diffCtx);
+  const contextStore = useContext(ctx);
   useEffect(() => {
-    if (!(ctx && context)) return;
-    if (context === "src") {
-      ctx.setSrc(args);
-    } else {
-      ctx.setDest(args);
+    if (contextStore && context) {
+      contextStore.setState({ [context]: args });
+      return () => {
+        contextStore.setState({ [context]: undefined });
+      };
     }
-  }, [args, context, ctx]);
+  });
 
   return <>{isLoading ? loading : `$${usdValue.toFixed(2)}`}</>;
 };
 
-type Context = {
-  src?: Args;
-  dest?: Args;
-  setSrc: (args: Args) => void;
-  setDest: (args: Args) => void;
-  reset: () => void;
+///////////////////////////////////////////////////////////////////////////////
+
+type Store = { src?: Args; dest?: Args };
+
+const createContextStore = () => {
+  return create<Store>(() => ({ src: undefined, dest: undefined }));
 };
 
-const diffCtx = createContext<Context>({
-  setSrc: () => {},
-  setDest: () => {},
-  reset: () => {},
-});
+type Context = ReturnType<typeof createContextStore> | undefined;
+const ctx = createContext<Context>(undefined);
+
+///////////////////////////////////////////////////////////////////////////////
 
 type UsdDiffValueProps = {
   src: Args;
   dest: Args;
-  loading?: ReactNode;
+  onLoading?: ReactNode;
+  onUndefined?: ReactNode;
   children?: (args: { isLoading: boolean; percentage: number }) => ReactNode;
 };
 
-export const UsdDiffValue = ({
-  src,
-  dest,
-  loading = "...",
-  children,
-}: UsdDiffValueProps) => {
+export const UsdDiffValue = (props: UsdDiffValueProps) => {
+  const { src, dest, onLoading = "...", children } = props;
   const { data: percentage = 0, isLoading } = useUsdDiffValue([src, dest]);
-  if (children) return <>{children({ isLoading, percentage })}</>;
-  if (isLoading) return <>{loading}</>;
+
+  if (children) {
+    return <>{children({ isLoading, percentage })}</>;
+  }
+  if (isLoading) {
+    return <>{onLoading}</>;
+  }
   return <>{percentage.toFixed(2)}%</>;
 };
 
 export const UsdDiff = {
   Provider: ({ children }: { children: ReactNode }) => {
-    const [src, setSrc] = useState<Args>();
-    const [dest, setDest] = useState<Args>();
-    const reset = () => (setSrc(undefined), setDest(undefined));
-    return (
-      <diffCtx.Provider value={{ src, dest, setSrc, setDest, reset }}>
-        {children}
-      </diffCtx.Provider>
-    );
+    const store = useRef<Context>(createContextStore());
+    return <ctx.Provider value={store.current}>{children}</ctx.Provider>;
   },
   Value: (props: Omit<UsdDiffValueProps, "src" | "dest">) => {
-    const ctx = useContext(diffCtx);
-    if (!(ctx?.src && ctx?.dest)) return null;
-    return <UsdDiffValue src={ctx.src} dest={ctx.dest} {...props} />;
+    const useStore = useContext(ctx);
+    if (!useStore) {
+      throw new Error("UsdDiff.Value must be used inside UsdDiff.Provider");
+    }
+
+    const { src, dest } = useStore();
+    if (!(src && dest)) {
+      return <>{props.onUndefined ?? null}</>;
+    }
+
+    return <UsdDiffValue src={src} dest={dest} {...props} />;
   },
 };
 
 export const useUsdDiffReset = () => {
-  const ctx = useContext(diffCtx);
-  return ctx?.reset || (() => {});
+  const store = useContext(ctx);
+  if (!store) {
+    throw new Error("useUsdDiffReset must be used inside UsdDiff.Provider");
+  }
+  return () => store.setState({ src: undefined, dest: undefined });
 };
