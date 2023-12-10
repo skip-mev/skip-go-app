a26625fe1e6bdda848ce47992da1c07c94784dd9
Griko Nibras <griko@nibras.co>
2023-12-10 08:08:31 +0700

    feat: add station, vectis, xdefi wallet integration [FRE-409, FRE-411, FRE-412]

--- src/lib/cosmos-kit.ts
+++ src/lib/cosmos-kit.ts
@@ -3,12 +3,18 @@ import { wallets as cosmostationWallets } from "@cosmos-kit/cosmostation";
 import { wallets as keplrWallets } from "@cosmos-kit/keplr";
 import { wallets as leapWallets } from "@cosmos-kit/leap";
 import { wallets as okxWallets } from "@cosmos-kit/okxwallet";
+import { wallets as stationWallets } from "@cosmos-kit/station";
+import { wallets as vectisWallets } from "@cosmos-kit/vectis";
+import { wallets as xdefiWallets } from "@cosmos-kit/xdefi";
 
 export const wallets = [
   ...keplrWallets,
   ...leapWallets,
   ...cosmostationWallets,
   ...okxWallets,
+  ...stationWallets,
+  ...vectisWallets,
+  ...xdefiWallets,
 ];
 
 export type MergedWalletClient =
@@ -16,4 +22,7 @@ export type MergedWalletClient =
   | import("@cosmos-kit/cosmostation-extension/cjs/extension/client").CosmostationClient
   | import("@cosmos-kit/keplr-extension/cjs/extension/client").KeplrClient
   | import("@cosmos-kit/leap-extension/cjs/extension/client").LeapClient
-  | import("@cosmos-kit/okxwallet-extension/cjs/extension/client").OkxwalletClient;
+  | import("@cosmos-kit/okxwallet-extension/cjs/extension/client").OkxwalletClient
+  | import("@cosmos-kit/station-extension/cjs/extension/client").StationClient
+  | import("@cosmos-kit/vectis-extension/cjs/extension/client").VectisClient
+  | import("@cosmos-kit/xdefi-extension/cjs/extension/client").XDEFIClient;
