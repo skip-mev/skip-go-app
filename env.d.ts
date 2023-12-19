1411cbe8b3cc8ad56b5ea46b930c058662f20012
Griko Nibras <griko@nibras.co>
2023-12-20 04:54:28 +0700

    feat: setup cors edge config

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly APP_URL?: string;
     readonly NEXT_PUBLIC_API_URL?: string;
+    readonly CORS_EDGE_CONFIG?: string;
     readonly POLKACHU_USER?: string;
     readonly POLKACHU_PASSWORD?: string;
   }
