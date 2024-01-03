aacb18b9ee2e8b7510dce26b540bfa783f26f4b0
Griko Nibras <griko@nibras.co>
2024-01-04 04:10:14 +0700

    chore: remove extended okx wallet wagmi connector
    Signed-off-by: Griko Nibras <griko@nibras.co>

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,10 +1,9 @@
 import { configureChains, createConfig } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { publicProvider } from "wagmi/providers/public";
 
 import { EVM_CHAINS } from "@/constants/constants";
-
-import { OkxWalletConnector } from "./wagmi/connectors";
 
 const { publicClient, chains } = configureChains(EVM_CHAINS, [
   publicProvider(),
@@ -14,7 +13,7 @@ export const wagmiConfig = createConfig({
   autoConnect: true,
   connectors: [
     new MetaMaskConnector({ chains }),
-    new OkxWalletConnector({ chains }),
+    new InjectedConnector({ chains }),
   ],
   publicClient,
 });
