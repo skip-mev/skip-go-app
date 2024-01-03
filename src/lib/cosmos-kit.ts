4ec4c38504ff0f086010122427c64431f71385e7
Griko Nibras <griko@nibras.co>
2024-01-04 04:09:30 +0700

    fix: update MergedWalletClient types
    Signed-off-by: Griko Nibras <griko@nibras.co>

--- src/lib/cosmos-kit.ts
+++ src/lib/cosmos-kit.ts
@@ -1,28 +1,31 @@
-import { WalletClient } from "@cosmos-kit/core";
-import { wallets as cosmostationWallets } from "@cosmos-kit/cosmostation";
-import { wallets as keplrWallets } from "@cosmos-kit/keplr";
-import { wallets as leapWallets } from "@cosmos-kit/leap";
-import { wallets as okxWallets } from "@cosmos-kit/okxwallet";
-import { wallets as stationWallets } from "@cosmos-kit/station";
-import { wallets as vectisWallets } from "@cosmos-kit/vectis";
-import { wallets as xdefiWallets } from "@cosmos-kit/xdefi";
+import { wallets as cosmostation } from "@cosmos-kit/cosmostation";
+import { wallets as keplr } from "@cosmos-kit/keplr";
+import { wallets as leap } from "@cosmos-kit/leap";
+import { wallets as okx } from "@cosmos-kit/okxwallet";
+import { wallets as station } from "@cosmos-kit/station";
+import { wallets as vectis } from "@cosmos-kit/vectis";
+import { wallets as xdefi } from "@cosmos-kit/xdefi";
 
 export const wallets = [
-  ...keplrWallets,
-  ...leapWallets,
-  ...cosmostationWallets,
-  ...okxWallets,
-  ...stationWallets,
-  ...vectisWallets,
-  ...xdefiWallets,
+  ...keplr,
+  ...leap,
+  ...cosmostation,
+  ...okx,
+  ...station,
+  ...vectis,
+  ...xdefi,
 ];
 
 export type MergedWalletClient =
-  | WalletClient
   | import("@cosmos-kit/cosmostation-extension/cjs/extension/client").CosmostationClient
+  | import("@cosmos-kit/cosmostation-mobile/cjs/wallet-connect/client").CosmostationClient
   | import("@cosmos-kit/keplr-extension/cjs/extension/client").KeplrClient
+  | import("@cosmos-kit/keplr-mobile/cjs/wallet-connect/client").KeplrClient
   | import("@cosmos-kit/leap-extension/cjs/extension/client").LeapClient
+  | import("@cosmos-kit/leap-metamask-cosmos-snap/cjs/extension/client").CosmosSnapClient
+  | import("@cosmos-kit/leap-mobile/cjs/wallet-connect/client").LeapClient
   | import("@cosmos-kit/okxwallet-extension/cjs/extension/client").OkxwalletClient
+  | import("@cosmos-kit/station-extension/cjs/extension/client").StationClient
   | import("@cosmos-kit/station-extension/cjs/extension/client").StationClient
   | import("@cosmos-kit/vectis-extension/cjs/extension/client").VectisClient
   | import("@cosmos-kit/xdefi-extension/cjs/extension/client").XDEFIClient;
