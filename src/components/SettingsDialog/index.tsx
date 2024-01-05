1771bcace01f09a6e70fd0646f5b90844eef0d40
Griko Nibras <griko@nibras.co>
2024-01-05 22:13:44 +0700

    feat: update settings dialog
    Signed-off-by: Griko Nibras <griko@nibras.co>

--- src/components/SettingsDialog/index.tsx
+++ src/components/SettingsDialog/index.tsx
@@ -4,6 +4,8 @@ import { tinykeys } from "tinykeys";
 
 import { useDisclosureKey } from "@/context/disclosures";
 
+import { AdaptiveLink } from "../AdaptiveLink";
+import { GasSetting } from "./GasSetting";
 import { SaveIndicator } from "./SaveIndicator";
 import { SlippageSetting } from "./SlippageSetting";
 
@@ -28,10 +30,22 @@ export const SettingsDialog = () => {
           <div className="flex-grow" />
           <SaveIndicator />
         </div>
+        <GasSetting />
+        <p className="p-2 text-sm text-neutral-500 [&_a]:text-red-500 [&_a:hover]:underline">
+          Gas is used to meter transactions and allocate resources fairly among
+          users. Users must pay a gas fee, usually in the native token, to have
+          their transactions processed by the network.{" "}
+          <AdaptiveLink href="https://docs.cosmos.network/main/glossary#gas">
+            (Learn more)
+          </AdaptiveLink>
+        </p>
         <SlippageSetting />
-        <p className="p-2 text-sm text-neutral-500">
+        <p className="p-2 text-sm text-neutral-500 [&_a]:text-red-500 [&_a:hover]:underline">
           Slippage is how much price movement you can tolerate between the time
-          you send out a transaction and the time it&apos;s executed.
+          you send out a transaction and the time it&apos;s executed.{" "}
+          <AdaptiveLink href="https://medium.com/onomy-protocol/what-is-slippage-in-defi-62a0d068feb3">
+            (Learn more)
+          </AdaptiveLink>
         </p>
       </div>
     </div>
