b1c8cea109e9b5bb9dbb3027e7d5f94145d0b601
Griko Nibras <griko@nibras.co>
2024-01-07 04:59:03 +0700

    feat: forward ref connected wallet button
    Signed-off-by: Griko Nibras <griko@nibras.co>

--- src/components/ConnectedWalletButton.tsx
+++ src/components/ConnectedWalletButton.tsx
@@ -1,27 +1,32 @@
-interface Props {
+import { clsx } from "clsx";
+import { ComponentProps, forwardRef } from "react";
+
+type Props = ComponentProps<"button"> & {
   address: string;
-  onClick: () => void;
   walletName: string;
   walletLogo?: string;
-}
+};
 
-export function ConnectedWalletButton({
-  address,
-  onClick,
-  walletLogo,
-  walletName,
-}: Props) {
-  return (
-    <button
-      className="flex items-center gap-2 border border-neutral-300 hover:bg-neutral-50 rounded-lg py-2 px-2.5 focus:outline-none transition-colors"
-      onClick={onClick}
-    >
-      {walletLogo && (
-        <img alt={walletName} className="w-4 h-4" src={walletLogo} />
-      )}
-      <span className="text-xs font-bold">
-        {address.slice(0, 8)}...{address.slice(-5)}
-      </span>
-    </button>
-  );
-}
+export const ConnectedWalletButton = forwardRef<HTMLButtonElement, Props>(
+  function Component(props, ref) {
+    const { address, walletLogo, walletName, ...rest } = props;
+    return (
+      <button
+        className={clsx(
+          "flex items-center gap-2 focus:outline-none transition-colors",
+          "border border-neutral-200 hover:bg-neutral-50 hover:border-neutral-300 rounded-lg py-2 px-2.5",
+        )}
+        {...rest}
+        ref={ref}
+      >
+        {walletLogo && (
+          <img alt={walletName} className="w-4 h-4" src={walletLogo} />
+        )}
+        <span className="text-xs font-bold">
+          {address.slice(0, 8)}...{address.slice(-5)}
+        </span>
+      </button>
+    );
+  },
+  //
+);
