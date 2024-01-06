e3c12c06b788949b9083ca31c93e07f134a207ff
Griko Nibras <griko@nibras.co>
2024-01-06 23:24:41 +0700

    chore: update tx toast error text
    Signed-off-by: Griko Nibras <griko@nibras.co>

--- src/components/TransactionDialog/TransactionDialogContent.tsx
+++ src/components/TransactionDialog/TransactionDialogContent.tsx
@@ -1,7 +1,7 @@
 import { ArrowLeftIcon, CheckCircleIcon } from "@heroicons/react/20/solid";
 import { RouteResponse } from "@skip-router/core";
 import { clsx } from "clsx";
-import { FC, Fragment, useState } from "react";
+import { FC, useState } from "react";
 import { toast } from "react-hot-toast";
 import { useAccount } from "wagmi";
 
@@ -172,7 +172,13 @@ const TransactionDialogContent: FC<Props> = ({
         console.error(err);
       }
       if (err instanceof Error) {
-        toast.error(err.message);
+        toast.error(
+          <p>
+            <strong>Swap Failed!</strong>
+            <br />
+            {err.name}: {err.message}
+          </p>,
+        );
       }
       failTxHistory(historyId);
       setTxStatuses((statuses) => {
@@ -208,166 +214,164 @@ const TransactionDialogContent: FC<Props> = ({
   }
 
   return (
-    <Fragment>
-      <div className="flex flex-col h-full px-4 py-6 space-y-6 overflow-y-auto scrollbar-hide">
-        <div>
-          <div className="flex items-center gap-4">
-            <button
-              className="hover:bg-neutral-100 w-8 h-8 rounded-full flex items-center justify-center transition-colors"
-              onClick={onClose}
-            >
-              <ArrowLeftIcon className="w-6 h-6" />
-            </button>
-            <p className="font-bold text-xl">Transaction Preview</p>
-          </div>
+    <div className="flex flex-col h-full px-4 py-6 space-y-6 overflow-y-auto scrollbar-hide">
+      <div>
+        <div className="flex items-center gap-4">
+          <button
+            className="hover:bg-neutral-100 w-8 h-8 rounded-full flex items-center justify-center transition-colors"
+            onClick={onClose}
+          >
+            <ArrowLeftIcon className="w-6 h-6" />
+          </button>
+          <p className="font-bold text-xl">Transaction Preview</p>
         </div>
-        <div className="border border-neutral-300 rounded-xl p-4">
-          <RouteDisplay route={route} />
-        </div>
-        <div className="bg-black text-white/50 font-medium uppercase text-xs p-3 rounded-md flex items-center w-full text-left">
-          <p className="flex-1">
-            This route requires{" "}
-            <span className="text-white">
-              {transactionCount} Transaction
-              {transactionCount > 1 ? "s" : ""}
-            </span>{" "}
-            to complete
-          </p>
-        </div>
-        <div className="flex-1 space-y-6">
-          {txStatuses.map(({ status, explorerLink, txHash }, i) => (
-            <div key={`tx-${i}`} className="flex items-center gap-4">
-              {status === "INIT" && (
-                <CheckCircleIcon className="text-neutral-300 w-7 h-7" />
-              )}
-              {status === "PENDING" && (
-                <svg
-                  className="animate-spin h-7 w-7 inline-block text-neutral-300"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
-              )}
-              {status === "SUCCESS" && (
-                <CheckCircleIcon className="text-emerald-400 w-7 h-7" />
-              )}
-              <div className="flex-1">
-                <p className="font-semibold">Transaction {i + 1}</p>
-              </div>
-              <div>
-                {txHash && explorerLink && (
-                  <a
-                    className="text-sm font-bold text-[#FF486E] hover:underline"
-                    href={explorerLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span>
-                      {txHash.slice(0, 6)}
-                      ...
-                      {txHash.slice(-6)}
-                    </span>
-                  </a>
-                )}
-              </div>
+      </div>
+      <div className="border border-neutral-300 rounded-xl p-4">
+        <RouteDisplay route={route} />
+      </div>
+      <div className="bg-black text-white/50 font-medium uppercase text-xs p-3 rounded-md flex items-center w-full text-left">
+        <p className="flex-1">
+          This route requires{" "}
+          <span className="text-white">
+            {transactionCount} Transaction
+            {transactionCount > 1 ? "s" : ""}
+          </span>{" "}
+          to complete
+        </p>
+      </div>
+      <div className="flex-1 space-y-6">
+        {txStatuses.map(({ status, explorerLink, txHash }, i) => (
+          <div key={`tx-${i}`} className="flex items-center gap-4">
+            {status === "INIT" && (
+              <CheckCircleIcon className="text-neutral-300 w-7 h-7" />
+            )}
+            {status === "PENDING" && (
+              <svg
+                className="animate-spin h-7 w-7 inline-block text-neutral-300"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            )}
+            {status === "SUCCESS" && (
+              <CheckCircleIcon className="text-emerald-400 w-7 h-7" />
+            )}
+            <div className="flex-1">
+              <p className="font-semibold">Transaction {i + 1}</p>
             </div>
-          ))}
-        </div>
-        <div className="space-y-4">
-          {transacting ? (
-            <button
-              className={clsx(
-                "bg-[#FF486E] text-white font-semibold py-4 rounded-md w-full",
-                "transition-transform outline-none",
-                "enabled:hover:scale-105 enabled:hover:rotate-1",
-                "disabled:cursor-not-allowed disabled:opacity-75",
-              )}
-              onClick={onClose}
-              disabled={route.txsRequired !== numberOfBroadcastedTransactions}
-            >
-              {route.txsRequired !== numberOfBroadcastedTransactions ? (
-                <svg
-                  className="animate-spin h-4 w-4 inline-block text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx={12}
-                    cy={12}
-                    r={10}
-                    stroke="currentColor"
-                    strokeWidth={4}
-                  />
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  />
-                </svg>
-              ) : (
-                <span>{route.doesSwap ? "Swap" : "Transfer"} Again</span>
-              )}
-            </button>
-          ) : (
-            <button
-              className={clsx(
-                "bg-[#FF486E] text-white font-semibold py-4 rounded-md w-full",
-                "transition-transform outline-none",
-                "enabled:hover:scale-105 enabled:hover:rotate-1",
-                "disabled:cursor-not-allowed disabled:opacity-75",
-              )}
-              onClick={onSubmit}
-              disabled={transacting || insufficentBalance}
-            >
-              Submit
-            </button>
-          )}
-          {insufficentBalance && !transacting && !txComplete && (
-            <p className="text-center font-semibold text-sm text-red-500">
-              Insufficient Balance
-            </p>
-          )}
-        </div>
-        {estimatedFinalityTime !== "" && (
-          <AlertCollapse.Root type="info">
-            <AlertCollapse.Trigger>
-              EVM bridging finality time is {estimatedFinalityTime}
-            </AlertCollapse.Trigger>
-            <AlertCollapse.Content>
-              <p>
-                This swap contains at least one EVM chain, so it might take
-                longer. Read more about{" "}
+            <div>
+              {txHash && explorerLink && (
                 <a
-                  href={HREF_COMMON_FINALITY_TIMES}
-                  className="underline"
+                  className="text-sm font-bold text-[#FF486E] hover:underline"
+                  href={explorerLink}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  common finality times
+                  <span>
+                    {txHash.slice(0, 6)}
+                    ...
+                    {txHash.slice(-6)}
+                  </span>
                 </a>
-                .
-              </p>
-            </AlertCollapse.Content>
-          </AlertCollapse.Root>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="space-y-4">
+        {transacting ? (
+          <button
+            className={clsx(
+              "bg-[#FF486E] text-white font-semibold py-4 rounded-md w-full",
+              "transition-transform outline-none",
+              "enabled:hover:scale-105 enabled:hover:rotate-1",
+              "disabled:cursor-not-allowed disabled:opacity-75",
+            )}
+            onClick={onClose}
+            disabled={route.txsRequired !== numberOfBroadcastedTransactions}
+          >
+            {route.txsRequired !== numberOfBroadcastedTransactions ? (
+              <svg
+                className="animate-spin h-4 w-4 inline-block text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx={12}
+                  cy={12}
+                  r={10}
+                  stroke="currentColor"
+                  strokeWidth={4}
+                />
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                />
+              </svg>
+            ) : (
+              <span>{route.doesSwap ? "Swap" : "Transfer"} Again</span>
+            )}
+          </button>
+        ) : (
+          <button
+            className={clsx(
+              "bg-[#FF486E] text-white font-semibold py-4 rounded-md w-full",
+              "transition-transform outline-none",
+              "enabled:hover:scale-105 enabled:hover:rotate-1",
+              "disabled:cursor-not-allowed disabled:opacity-75",
+            )}
+            onClick={onSubmit}
+            disabled={transacting || insufficentBalance}
+          >
+            Submit
+          </button>
+        )}
+        {insufficentBalance && !transacting && !txComplete && (
+          <p className="text-center font-semibold text-sm text-red-500">
+            Insufficient Balance
+          </p>
         )}
       </div>
-    </Fragment>
+      {estimatedFinalityTime !== "" && (
+        <AlertCollapse.Root type="info">
+          <AlertCollapse.Trigger>
+            EVM bridging finality time is {estimatedFinalityTime}
+          </AlertCollapse.Trigger>
+          <AlertCollapse.Content>
+            <p>
+              This swap contains at least one EVM chain, so it might take
+              longer. Read more about{" "}
+              <a
+                href={HREF_COMMON_FINALITY_TIMES}
+                className="underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                common finality times
+              </a>
+              .
+            </p>
+          </AlertCollapse.Content>
+        </AlertCollapse.Root>
+      )}
+    </div>
   );
 };
 
