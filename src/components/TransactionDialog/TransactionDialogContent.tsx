ffbe8d185fe1e767c51e0220aeccf94520f5f2e5
Griko Nibras <griko@nibras.co>
2024-01-07 13:33:35 +0700

    feat: extract spinner icon

--- src/components/TransactionDialog/TransactionDialogContent.tsx
+++ src/components/TransactionDialog/TransactionDialogContent.tsx
@@ -21,6 +21,7 @@ import { getChainExplorerUrl } from "@/utils/explorer";
 import { getOfflineSigner } from "@/utils/signer";
 
 import RouteDisplay from "../RouteDisplay";
+import { SpinnerIcon } from "../SpinnerIcon";
 import TransactionSuccessView from "../TransactionSuccessView";
 import * as AlertCollapse from "./AlertCollapse";
 
@@ -237,26 +238,7 @@ function TransactionDialogContent({
               <CheckCircleIcon className="text-neutral-300 w-7 h-7" />
             )}
             {status === "PENDING" && (
-              <svg
-                className="animate-spin h-7 w-7 inline-block text-neutral-300"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
+              <SpinnerIcon className="animate-spin h-7 w-7 inline-block text-neutral-300" />
             )}
             {status === "SUCCESS" && (
               <CheckCircleIcon className="text-emerald-400 w-7 h-7" />
