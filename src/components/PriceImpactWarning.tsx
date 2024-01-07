77fd37b0383199a7a17f0e596a064098ddca9f32
Griko Nibras <griko@nibras.co>
2024-01-07 14:37:50 +0700

    feat: update price impact dialog [FRE-479]
    https://linear.app/skip/issue/FRE-479/switch-price-impact-warning-cta-buttons
    Signed-off-by: Griko Nibras <griko@nibras.co>

--- src/components/PriceImpactWarning.tsx
+++ src/components/PriceImpactWarning.tsx
@@ -1,3 +1,5 @@
+import { ExclamationTriangleIcon } from "@heroicons/react/20/solid";
+
 import { useDisclosureKey } from "@/context/disclosures";
 
 interface Props {
@@ -16,49 +18,34 @@ export const PriceImpactWarning = ({
   if (!isOpen || title === "") return null;
 
   return (
-    <div className="absolute inset-0 bg-white rounded-3xl">
-      <div className="h-full px-4 py-6 overflow-y-auto scrollbar-hide">
-        <div className="h-full flex flex-col">
-          <div className="flex-1 pt-8">
-            <div className="text-red-400 flex justify-center py-16">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="h-24 w-24"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <p className="font-bold text-lg text-center text-red-500 mb-2">
-              {title}
-            </p>
-            <p className="text-center text-lg px-4 leading-snug text-neutral-500">
-              {message} Do you want to continue?
-            </p>
-          </div>
-          <div className="flex items-end gap-2">
-            <button
-              className="bg-[#FF486E] hover:bg-[#ed1149] transition-colors text-white font-semibold py-4 rounded-md w-full"
-              onClick={() => {
-                control.close();
-                onGoBack();
-              }}
-            >
-              Go Back
-            </button>
-            <button
-              className="border border-neutral-400 text-neutral-500 font-semibold py-4 rounded-md w-full transition-colors hover:bg-neutral-50"
-              onClick={() => control.close()}
-            >
-              Continue
-            </button>
-          </div>
+    <div className="absolute inset-0 bg-white rounded-3xl p-6 overflow-y-auto scrollbar-hide flex flex-col">
+      <div className="flex-grow pt-8">
+        <div className="text-red-400 flex justify-center py-16">
+          <ExclamationTriangleIcon className="h-24 w-24" />
         </div>
+        <h3 className="font-bold text-lg text-center text-red-500 mb-2 tabular-nums">
+          {title}
+        </h3>
+        <p className="text-center text-lg px-4 leading-snug text-neutral-500 tabular-nums">
+          {message} Do you want to continue?
+        </p>
+      </div>
+      <div className="flex items-end gap-2">
+        <button
+          className="border border-neutral-400 text-neutral-500 font-semibold py-4 rounded-lg w-full transition-colors hover:bg-neutral-100"
+          onClick={() => control.close()}
+        >
+          Continue
+        </button>
+        <button
+          className="bg-[#FF486E] hover:bg-[#ed1149] transition-colors text-white font-semibold py-4 rounded-lg w-full border border-transparent"
+          onClick={() => {
+            control.close();
+            onGoBack();
+          }}
+        >
+          Go Back
+        </button>
       </div>
     </div>
   );
