fda14407e4e0e12b5c604d0d91d70741f8f16392
Griko Nibras <griko@nibras.co>
2024-01-09 02:58:44 +0700

    chore: swap dialog divs with radix dialog
    Signed-off-by: Griko Nibras <griko@nibras.co>

--- src/components/JsonDialog/index.tsx
+++ src/components/JsonDialog/index.tsx
@@ -3,6 +3,7 @@ import {
   CheckIcon,
   ClipboardDocumentIcon,
 } from "@heroicons/react/20/solid";
+import * as Dialog from "@radix-ui/react-dialog";
 import { clsx } from "clsx";
 import { useMemo, useState } from "react";
 
@@ -28,38 +29,42 @@ export const JsonDialog = () => {
   if (!state) return null;
 
   return (
-    <div className="absolute inset-0 bg-white rounded-3xl overflow-hidden">
-      <button
-        className={clsx(
-          "absolute top-5 right-4",
-          "text-sm px-2 py-1 border rounded-lg transition-colors",
-          "flex items-center justify-center space-x-1 flex-grow",
-          {
-            "bg-neutral-100 hover:bg-neutral-200": !copied,
-            "bg-green-100 hover:bg-green-200": copied,
-            "border-green-400 text-green-900": copied,
-          },
-        )}
-        onClick={onCopy}
-      >
-        <span>Copy to clipboard</span>
-        <ClipboardIcon className="w-4 h-4" />
-      </button>
-      <div className="h-full px-4 py-6 overflow-y-auto scrollbar-hide">
-        <div className="flex items-center gap-4 pb-2">
-          <button
-            className="hover:bg-neutral-100 w-8 h-8 rounded-full flex items-center justify-center transition-colors"
-            onClick={close}
-          >
-            <ArrowLeftIcon className="w-6 h-6" />
-          </button>
-          <h3 className="font-bold text-xl">{state.title || "JSON Viewer"}</h3>
-          <div className="flex-grow" />
+    <Dialog.Root modal open>
+      <Dialog.Content className="absolute inset-0 bg-white rounded-3xl overflow-hidden">
+        <button
+          className={clsx(
+            "absolute top-5 right-4",
+            "text-sm px-2 py-1 border rounded-lg transition-colors",
+            "flex items-center justify-center space-x-1 flex-grow",
+            {
+              "bg-neutral-100 hover:bg-neutral-200": !copied,
+              "bg-green-100 hover:bg-green-200": copied,
+              "border-green-400 text-green-900": copied,
+            },
+          )}
+          onClick={onCopy}
+        >
+          <span>Copy to clipboard</span>
+          <ClipboardIcon className="w-4 h-4" />
+        </button>
+        <div className="h-full px-4 py-6 overflow-y-auto scrollbar-hide">
+          <div className="flex items-center gap-4 pb-2">
+            <button
+              className="hover:bg-neutral-100 w-8 h-8 rounded-full flex items-center justify-center transition-colors"
+              onClick={close}
+            >
+              <ArrowLeftIcon className="w-6 h-6" />
+            </button>
+            <h3 className="font-bold text-xl">
+              {state.title || "JSON Viewer"}
+            </h3>
+            <div className="flex-grow" />
+          </div>
+          <pre className="border font-mono p-2 rounded-lg text-xs overflow-auto">
+            {JSON.stringify(state.data, null, 2)}
+          </pre>
         </div>
-        <pre className="border font-mono p-2 rounded-lg text-xs overflow-auto">
-          {JSON.stringify(state.data, null, 2)}
-        </pre>
-      </div>
-    </div>
+      </Dialog.Content>
+    </Dialog.Root>
   );
 };
--- src/components/SettingsDialog/index.tsx
+++ src/components/SettingsDialog/index.tsx
@@ -2,8 +2,7 @@ import {
   ArrowLeftIcon,
   QuestionMarkCircleIcon,
 } from "@heroicons/react/16/solid";
-import { useEffect } from "react";
-import { tinykeys } from "tinykeys";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import { useDisclosureKey } from "@/context/disclosures";
 
@@ -14,43 +13,40 @@ import { SlippageSetting } from "./SlippageSetting";
 
 export const SettingsDialog = () => {
   const [isOpen, { close }] = useDisclosureKey("settingsDialog");
-
-  useEffect(() => tinykeys(window, { ["Escape"]: () => close() }), [close]);
-
-  if (!isOpen) return null;
-
   return (
-    <div className="absolute inset-0 bg-white rounded-3xl animate-fade-zoom-in">
-      <div className="h-full px-4 py-6 overflow-y-auto scrollbar-hide">
-        <div className="flex items-center gap-4 pb-2">
-          <button
-            className="hover:bg-neutral-100 w-8 h-8 rounded-full flex items-center justify-center transition-colors"
-            onClick={close}
-          >
-            <ArrowLeftIcon className="w-6 h-6" />
-          </button>
-          <h3 className="font-bold text-xl">Swap Settings</h3>
-          <div className="flex-grow" />
-          <SaveIndicator />
+    <Dialog.Root modal open={isOpen}>
+      <Dialog.Content className="absolute inset-0 bg-white rounded-3xl animate-fade-zoom-in">
+        <div className="h-full px-4 py-6 overflow-y-auto scrollbar-hide">
+          <div className="flex items-center gap-4 pb-2">
+            <button
+              className="hover:bg-neutral-100 w-8 h-8 rounded-full flex items-center justify-center transition-colors"
+              onClick={close}
+            >
+              <ArrowLeftIcon className="w-6 h-6" />
+            </button>
+            <h3 className="font-bold text-xl">Swap Settings</h3>
+            <div className="flex-grow" />
+            <SaveIndicator />
+          </div>
+          <GasSetting />
+          <p className="p-2 text-sm text-neutral-500 [&_a]:text-red-500 [&_a:hover]:underline">
+            Gas is used to meter transactions and allocate resources fairly
+            among users. Users must pay a gas fee, usually in the native token,
+            to have their transactions processed by the network.{" "}
+            <AdaptiveLink href="https://docs.cosmos.network/main/glossary#gas">
+              <QuestionMarkCircleIcon className="w-3.5 h-3.5 inline mb-1.5" />
+            </AdaptiveLink>
+          </p>
+          <SlippageSetting />
+          <p className="p-2 text-sm text-neutral-500 [&_a]:text-red-500 [&_a:hover]:underline">
+            Slippage is how much price movement you can tolerate between the
+            time you send out a transaction and the time it&apos;s executed.{" "}
+            <AdaptiveLink href="https://medium.com/onomy-protocol/what-is-slippage-in-defi-62a0d068feb3">
+              <QuestionMarkCircleIcon className="w-3.5 h-3.5 inline mb-1.5" />
+            </AdaptiveLink>
+          </p>
         </div>
-        <GasSetting />
-        <p className="p-2 text-sm text-neutral-500 [&_a]:text-red-500 [&_a:hover]:underline">
-          Gas is used to meter transactions and allocate resources fairly among
-          users. Users must pay a gas fee, usually in the native token, to have
-          their transactions processed by the network.{" "}
-          <AdaptiveLink href="https://docs.cosmos.network/main/glossary#gas">
-            <QuestionMarkCircleIcon className="w-3.5 h-3.5 inline mb-1.5" />
-          </AdaptiveLink>
-        </p>
-        <SlippageSetting />
-        <p className="p-2 text-sm text-neutral-500 [&_a]:text-red-500 [&_a:hover]:underline">
-          Slippage is how much price movement you can tolerate between the time
-          you send out a transaction and the time it&apos;s executed.{" "}
-          <AdaptiveLink href="https://medium.com/onomy-protocol/what-is-slippage-in-defi-62a0d068feb3">
-            <QuestionMarkCircleIcon className="w-3.5 h-3.5 inline mb-1.5" />
-          </AdaptiveLink>
-        </p>
-      </div>
-    </div>
+      </Dialog.Content>
+    </Dialog.Root>
   );
 };
