d42c92c39aadf20ea84b118f2c70feb212b9457b
Griko Nibras <griko@nibras.co>
2024-01-10 03:42:43 +0700

    chore: update settings help links
    Signed-off-by: Griko Nibras <griko@nibras.co>

--- src/components/SettingsDialog/index.tsx
+++ src/components/SettingsDialog/index.tsx
@@ -1,7 +1,4 @@
-import {
-  ArrowLeftIcon,
-  QuestionMarkCircleIcon,
-} from "@heroicons/react/16/solid";
+import { ArrowLeftIcon } from "@heroicons/react/16/solid";
 import * as Dialog from "@radix-ui/react-dialog";
 
 import { useDisclosureKey } from "@/context/disclosures";
@@ -29,20 +26,22 @@ export const SettingsDialog = () => {
             <SaveIndicator />
           </div>
           <GasSetting />
-          <p className="p-2 text-sm text-neutral-500 [&_a]:text-red-500 [&_a:hover]:underline">
+          <p className="p-2 text-sm text-neutral-500 [&_a]:text-red-500 [&_a:hover]:underline text-balance">
             Gas is used to meter transactions and allocate resources fairly
             among users. Users must pay a gas fee, usually in the native token,
-            to have their transactions processed by the network.{" "}
+            to have their transactions processed by the network.
+            <br />
             <AdaptiveLink href="https://docs.cosmos.network/main/glossary#gas">
-              <QuestionMarkCircleIcon className="w-3.5 h-3.5 inline mb-1.5" />
+              Learn more
             </AdaptiveLink>
           </p>
           <SlippageSetting />
-          <p className="p-2 text-sm text-neutral-500 [&_a]:text-red-500 [&_a:hover]:underline">
+          <p className="p-2 text-sm text-neutral-500 [&_a]:text-red-500 [&_a:hover]:underline text-balance">
             Slippage is how much price movement you can tolerate between the
-            time you send out a transaction and the time it&apos;s executed.{" "}
+            time you send out a transaction and the time it&apos;s executed.
+            <br />
             <AdaptiveLink href="https://medium.com/onomy-protocol/what-is-slippage-in-defi-62a0d068feb3">
-              <QuestionMarkCircleIcon className="w-3.5 h-3.5 inline mb-1.5" />
+              Learn more
             </AdaptiveLink>
           </p>
         </div>
