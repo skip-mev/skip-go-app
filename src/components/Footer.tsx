4b7a1c66eeaf2349b087572974e49d61f53302ec
Griko Nibras <griko@nibras.co>
2024-01-23 01:35:45 +0700

    chore: update footer
    Signed-off-by: Griko Nibras <griko@nibras.co>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,15 +6,10 @@ import { useDisclosureKey } from "@/context/disclosures";
 export function Footer() {
   const [isOpen, { open }] = useDisclosureKey("contactDialog");
   return (
-    <footer
-      className={clsx(
-        "z-10 pt-8",
-        "sm:fixed sm:bottom-0 sm:right-4",
-        //
-      )}
-    >
+    <footer className="w-full self-stretch pt-16">
       <button
         className={clsx(
+          "bottom-0 right-4 sm:fixed",
           "group flex items-center gap-2 rounded-t-lg px-4 py-2 sm:shadow-xl",
           "bg-white text-[#FF486E] hover:bg-red-50 sm:hover:pb-3 sm:active:pb-2.5",
           "transition-[background,padding,transform] duration-500 ease-[cubic-bezier(0.08,0.82,0.17,1)]",
