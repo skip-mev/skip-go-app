72bb2e4aea020a87e4adc00085b5ccb4363b94b2
Griko Nibras <griko@nibras.co>
2024-01-27 04:41:22 +0700

    fix: typo

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -2,5 +2,5 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(cn(inputs));
+  return twMerge(clsx(inputs));
 }
