2df09fc071ae7d02ec44b9b67de0571c50568a10
Nur Fikri <codingki@gmail.com>
2024-02-16 06:46:46 +0700

    refactor: extract component

--- src/components/RouteDisplay.tsx
+++ src/components/RouteDisplay.tsx
@@ -9,6 +9,7 @@ import { useBroadcastedTxsStatus } from "@/solve";
 
 import { AdaptiveLink } from "./AdaptiveLink";
 import { Gap } from "./common/Gap";
+import { ExpandArrow } from "./Icons/ExpandArrow";
 import { Step } from "./RouteDisplay/Step";
 import { SimpleTooltip } from "./SimpleTooltip";
 import { BroadcastedTx } from "./TransactionDialog/TransactionDialogContent";
@@ -153,7 +154,7 @@ function TransferStep({ action, actions, id, statusData }: TransferStepProps) {
         return <Step.LoadingState />;
 
       default:
-        return <div className="h-2 w-2 rounded-full bg-neutral-200" />;
+        return <Step.DefaultState />;
     }
   }, [isFirstOpSwap, transferStatus?.state]);
 
@@ -359,7 +360,7 @@ function SwapStep({ action, actions, id, statusData }: SwapStepProps) {
         return <Step.FailureState />;
       }
 
-      return <div className="h-2 w-2 rounded-full bg-neutral-200" />;
+      return <Step.DefaultState />;
     }
     switch (swapStatus?.state) {
       case "TRANSFER_RECEIVED":
@@ -702,18 +703,7 @@ function RouteDisplay({ route, isRouteExpanded, setIsRouteExpanded, broadcastedT
               className="rounded-full border-2 border-neutral-200 bg-white p-1 text-neutral-400 transition-transform hover:scale-110"
               onClick={() => setIsRouteExpanded(true)}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-4 w-4"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <ExpandArrow className="h-4 w-4" />
             </button>
           </div>
         )}
