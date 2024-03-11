d06ee8225f1c1805e3e4a5530e161975234fff30
Nur Fikri <codingki@gmail.com>
2024-03-12 04:27:56 +0700

    disable cosmos wallets on mobile

--- src/lib/cosmos-kit.ts
+++ src/lib/cosmos-kit.ts
@@ -1,7 +1,7 @@
 import { WalletClient } from "@cosmos-kit/core";
-import { wallets as cosmostation } from "@cosmos-kit/cosmostation";
-import { wallets as keplr } from "@cosmos-kit/keplr";
-import { wallets as leap } from "@cosmos-kit/leap";
+import { wallets as cosmostation } from "@cosmos-kit/cosmostation-extension";
+import { wallets as keplr } from "@cosmos-kit/keplr-extension";
+import { wallets as leap } from "@cosmos-kit/leap-extension";
 import { wallets as okxwallet } from "@cosmos-kit/okxwallet";
 import { wallets as station } from "@cosmos-kit/station";
 import { wallets as vectis } from "@cosmos-kit/vectis";
