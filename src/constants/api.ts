561872df90a1de0b2ceaa3075cc2b3236afa4d4b
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2024-05-30 19:23:19 +0200

    feat: use k8s dev

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,5 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.skip.money";
+// export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.skip.money";
+export const API_URL = "https://api.dev.skip.money";
 
 export const APP_URL = process.env.APP_URL;
 
