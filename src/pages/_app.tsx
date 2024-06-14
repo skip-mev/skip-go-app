7ed0af9f62f99601c5c3df2bce24f92b853bfc58
Nur Fikri <codingki@gmail.com>
2024-06-15 06:59:58 +0700

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,19 @@
 import "@/styles/globals.css";
 
+import { SwapWidgetProvider } from "@skip-go/widget";
 import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
 
 import { DefaultSeo } from "@/components/DefaultSeo";
-import { Provider } from "@/widget";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo />
       <Analytics />
-      <Provider>
+      <SwapWidgetProvider>
         <Component {...pageProps} />
-      </Provider>
+      </SwapWidgetProvider>
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
+import { SwapWidget } from "@skip-go/widget";
+
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Help } from "@/components/Help";
 import SkipBanner from "@/components/SkipBanner";
 import { VersionCheck } from "@/components/VersionCheck";
 import { cn } from "@/utils/ui";
-import { Widget } from "@/widget";
 
 export default function Home() {
   return (
@@ -17,11 +18,11 @@ export default function Home() {
       )}
     >
       <main className="relative flex min-h-screen flex-col items-center sm:pt-11">
-        <SkipBanner className="inset-x-0 top-0 z-50 w-screen sm:fixed" />
+        <SkipBanner className="z-9 inset-x-0 top-0 w-screen sm:fixed" />
         <Header />
         <div className="flex flex-grow flex-col items-center">
-          <div className="relative w-screen overflow-hidden bg-white p-6 shadow-xl sm:max-w-[450px] sm:rounded-3xl">
-            <Widget />
+          <div className="relative w-screen overflow-hidden bg-white p-2 shadow-xl sm:max-w-[450px] sm:rounded-3xl">
+            <SwapWidget className="" />
           </div>
           <Footer />
         </div>
