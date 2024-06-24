2115398b0ffcb4a88ac7f14f0127c3c0ea67ecf5
Nur Fikri <codingki@gmail.com>
2024-06-25 02:22:29 +0700

    fix: logo size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <nav className="relative flex items-center justify-center px-4 py-8 sm:py-12">
       <AdaptiveLink href="/">
-        <IbcFunLogo className="h-16 sm:h-16" />
+        <IbcFunLogo className="h-16" />
       </AdaptiveLink>
     </nav>
   );
