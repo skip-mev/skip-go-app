3bf75600c2a4ba4c5f763b3de29db47586c5a82f
Todd Kao <toddkao@users.noreply.github.com>
2024-07-23 22:03:57 -0400

    Test with SwapWidget instead of SwapWidgetProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,16 @@
 import "@/styles/globals.css";
 
-import { SwapWidgetProvider } from "@skip-go/widget";
 import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
 
 import { DefaultSeo } from "@/components/DefaultSeo";
-import { apiURL, endpointOptions } from "@/lib/skip-go-widget";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo />
       <Analytics />
-      <SwapWidgetProvider
-        endpointOptions={endpointOptions}
-        apiURL={apiURL}
-      >
-        <Component {...pageProps} />
-      </SwapWidgetProvider>
+      <Component {...pageProps} />
     </>
   );
 }
--- src/pages/widget.tsx
+++ src/pages/widget.tsx
@@ -1,21 +1,16 @@
-import { SwapWidgetWithoutProviders } from "@skip-go/widget";
+import { SwapWidget } from "@skip-go/widget";
 
 import { useURLQueryParams } from "@/hooks/useURLQueryParams";
+import { apiURL, endpointOptions } from "@/lib/skip-go-widget";
 
 export default function WidgetPage() {
-  const defaultRoute = useURLQueryParams();
+  // const defaultRoute = useURLQueryParams();
   return (
     <div className="relative bg-white p-6 scrollbar-hide">
-      <SwapWidgetWithoutProviders
+      <SwapWidget
         className=""
-        defaultRoute={{
-          srcChainID: defaultRoute?.srcChain,
-          srcAssetDenom: defaultRoute?.srcAssetDenom,
-          destChainID: defaultRoute?.destChain,
-          destAssetDenom: defaultRoute?.destAssetDenom,
-          amountIn: Number(defaultRoute?.amountIn),
-          amountOut: Number(defaultRoute?.amountOut),
-        }}
+        endpointOptions={endpointOptions}
+        apiURL={apiURL}
         settings={{
           customGasAmount: 200_000,
           slippage: 3,
