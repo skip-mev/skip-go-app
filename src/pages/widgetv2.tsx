a59f7e47393c2c5badcf1ab9dabc256ff3371b3e
Todd Kao <todd.kakao@gmail.com>
2024-10-25 11:20:03 -0400

    Add auto detecting theme

--- src/pages/widgetv2.tsx
+++ src/pages/widgetv2.tsx
@@ -1,28 +1,44 @@
-import { SwapWidget } from "widgetv2";
+import { defaultTheme, lightTheme, SwapWidget } from "widgetv2";
 
 import DiscordButton from "@/components/DiscordButton";
 import { LogoGo } from "@/components/LogoGo";
 import { VersionCheck } from "@/components/VersionCheck";
 import WidgetButton from "@/components/WidgetButton";
-import { useURLQueryParams } from "@/hooks/useURLQueryParams";
 import { cn } from "@/utils/ui";
 
 import { apiURL, endpointOptions } from "@/lib/skip-go-widget";
+import { useEffect, useState } from "react";
 
 export default function WidgetV2() {
-  const defaultRoute = useURLQueryParams();
+  const [theme, setTheme] = useState('dark');
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+        setTheme('light');
+      } else {
+        setTheme('dark');
+      }
+    
+      window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', event => {
+        const newColorScheme = event.matches ? "light" : "dark";
+        setTheme(newColorScheme);
+        console.log(newColorScheme);
+      });
+    }
+  }, []);
   return (
     <div
       className={cn(
         "bg-[#ff86ff] font-sans subpixel-antialiased",
         "relative overflow-x-hidden overflow-y-hidden",
         "before:fixed before:inset-x-0 before:bottom-0 before:h-[100vh] before:content-['']",
-        "before:bg-[url(/widgetv2-bg.svg)] before:bg-cover before:bg-[center_top] before:bg-no-repeat",
+        "before:bg-cover before:bg-[center_top] before:bg-no-repeat",
+        theme === 'dark' ? 'before:bg-[url(/widgetv2-dark-bg.svg)]' : 'before:bg-[url(/widgetv2-light-bg.svg)]'
       )}
     >
       <main className="relative flex min-h-screen flex-col items-center">
         <div className="flex h-20 w-full flex-row items-center justify-between px-6 py-4">
-          <LogoGo color="white" />
+          <LogoGo color={theme === "dark" ? 'white' : 'black'} />
           <div className="flex flex-row space-x-2">
             <WidgetButton />
             <DiscordButton />
@@ -37,7 +53,10 @@ export default function WidgetV2() {
             }}
           >
             <SwapWidget
-              theme={{ brandColor: "#FF4FFF" }}
+              theme={theme === 'dark' ? defaultTheme : {
+                ...lightTheme,
+                brandColor: "#FF4FFF",
+              }}
               endpointOptions={endpointOptions}
               apiURL={apiURL}
             />
