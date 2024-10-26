81a4af66be63dfb85bf7128611a23460053d58fc
Todd Kao <todd.kakao@gmail.com>
2024-10-25 20:03:55 -0400

    Add provider back

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,22 @@ import "@/styles/globals.css";
 
 import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
+import { SwapWidgetProvider } from "widgetv1";
 
 import { DefaultSeo } from "@/components/DefaultSeo";
+import { apiURL, endpointOptions } from "@/lib/skip-go-widget";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo />
       <Analytics />
-      <Component {...pageProps} />
+      <SwapWidgetProvider
+        endpointOptions={endpointOptions}
+        apiURL={apiURL}
+      >
+        <Component {...pageProps} />
+      </SwapWidgetProvider>
     </>
   );
 }
