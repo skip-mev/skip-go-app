14e45f1bc375b4ee3d89efc192939969972774d6
plubber <51789398+ericHgorski@users.noreply.github.com>
2024-12-12 13:12:08 -0500

    fix: update TryAPIButton link to point to the documentation

--- src/components/TryAPIButton.tsx
+++ src/components/TryAPIButton.tsx
@@ -4,7 +4,7 @@ import styles from "./button.module.css";
 const TryAPIButton = () => (
   <a
     className={`${styles.skipbutton} font-diatype`}
-    href="https://go.skip.build/"
+    href="https://docs.skip.build/go"
     target="_blank"
   >
     Try the Skip:Go API
