fe12bdaaaad821ca57a03ebe85ce139fcdb603d4
Nur Fikri <codingki@gmail.com>
2025-01-07 04:25:47 +0700

    fix: codegen

--- src/ast/parse-chain-paths.ts
+++ src/ast/parse-chain-paths.ts
@@ -39,6 +39,8 @@ export async function parseChainPaths({
       parseAssetListJson({ registryPath: _registryPath, chainPath }),
       parseChainJson({ registryPath: _registryPath, chainPath }),
     ]);
+    if (!assetlist) return;
+    if (!chain) return;
 
     chains.push(chain);
     assetlists.push(assetlist);
