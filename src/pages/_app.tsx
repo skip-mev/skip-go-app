3f1799f33d75fc671436c45f2943b646106d66d7
plubber <51789398+ericHgorski@users.noreply.github.com>
2025-03-18 15:57:44 -0400

    Add @next/third-parties package and replace Google Tag Manager script with component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import "@/styles/globals.css";
 
+import { GoogleTagManager } from "@next/third-parties/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
-import Script from "next/script";
 import React from "react";
 
 import { DefaultSeo } from "@/components/DefaultSeo";
@@ -14,19 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo />
-      <Script
-        id="gtm-script"
-        strategy="afterInteractive"
-      >
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5XMZ695Z');
-        `}
-      </Script>
-
+      <GoogleTagManager gtmId="GTM-5XMZ695Z" />
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
