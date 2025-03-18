83611a4d460010caa862df20f769bac5e1633743
plubber <51789398+ericHgorski@users.noreply.github.com>
2025-03-18 15:54:58 -0400

    Add Google Tag Manager script to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
+import Script from "next/script";
 import React from "react";
 
 import { DefaultSeo } from "@/components/DefaultSeo";
@@ -13,10 +14,24 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo />
-      <Analytics />
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+      >
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5XMZ695Z');
+        `}
+      </Script>
+
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
+
+      <Analytics />
     </>
   );
 }
