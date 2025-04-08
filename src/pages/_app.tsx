d87b4901a5e2cca839b1269b79c4eea2443da416
Todd Kao <todd.kakao@gmail.com>
2025-04-08 11:45:52 -0400

    remove public/favicon and move handling favicon to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { GoogleTagManager } from "@next/third-parties/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 import React from "react";
 
 import { DefaultSeo } from "@/components/DefaultSeo";
@@ -15,6 +16,12 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Head>
+        <link
+          rel="icon"
+          href={isCosmosDomain ? "/cosmos-favicon.svg" : "/skip-favicon.ico"}
+        />
+      </Head>
       <DefaultSeo />
       <GoogleTagManager gtmId="GTM-5XMZ695Z" />
       <QueryClientProvider client={queryClient}>
