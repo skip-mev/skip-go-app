c052d2757897f1502e386ea41c3a3e5da8779377
Nur Fikri <codingki@gmail.com>
2025-04-10 18:43:48 +0700

    release 3.6.8

--- src/components/pages/cosmos.tsx
+++ src/components/pages/cosmos.tsx
@@ -12,7 +12,6 @@ import { Banner } from "../Banner";
 import cosmosStyles from "../cosmos/cosmos.module.css";
 import { CosmosIcon } from "../cosmos/CosmosIcon";
 import { ShareIcon } from "../cosmos/ShareIcon";
-import { ThinArrowIcon } from "../cosmos/ThinArrowIcon";
 
 export function CosmosPage() {
   const defaultRoute = useURLQueryParams();
@@ -113,12 +112,16 @@ export function CosmosPage() {
               }}
               filterOut={{
                 source: {
-                  ...(showLedgerMainnet ? {} : { "ledger-mainnet-1": undefined }),
+                  ...(showLedgerMainnet
+                    ? {}
+                    : { "ledger-mainnet-1": undefined, 1: ["0xbf45a5029d081333407cc52a84be5ed40e181c46"] }),
                 },
                 destination: {
                   "pacific-1": ["ibc/6C00E4AA0CC7618370F81F7378638AE6C48EFF8C9203CE1C2357012B440EBDB7"],
                   "1329": ["0xB75D0B03c06A926e488e2659DF1A861F860bD3d1"],
-                  ...(showLedgerMainnet ? {} : { "ledger-mainnet-1": undefined }),
+                  ...(showLedgerMainnet
+                    ? {}
+                    : { "ledger-mainnet-1": undefined, 1: ["0xbf45a5029d081333407cc52a84be5ed40e181c46"] }),
                 },
               }}
               disableShadowDom
