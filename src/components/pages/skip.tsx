af0bd02a1d6543387b70c4df2cd60042b8349c3c
plubber <51789398+ericHgorski@users.noreply.github.com>
2025-04-10 10:24:40 -0400

    refactor: update filterOut logic in CosmosPage and SkipPage components

--- src/components/pages/skip.tsx
+++ src/components/pages/skip.tsx
@@ -88,7 +88,8 @@ export function SkipPage() {
               }}
               filterOut={{
                 source: {
-                  ...(showLedgerMainnet ? {} : { "ledger-mainnet-1": undefined, 1: ["0xFEC6a341F9B7e30E30Ef5B990158FA539B6bb057"] }),
+                  1: ["0xFEC6a341F9B7e30E30Ef5B990158FA539B6bb057"],
+                  ...(showLedgerMainnet ? {} : { "ledger-mainnet-1": undefined }),
                 },
                 destination: {
                   "pacific-1": ["ibc/6C00E4AA0CC7618370F81F7378638AE6C48EFF8C9203CE1C2357012B440EBDB7"],
