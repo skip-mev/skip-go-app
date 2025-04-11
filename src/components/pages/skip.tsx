afacadc8432c5900e9f1a8e400a029fd38e48d80
Todd Kao <todd.kakao@gmail.com>
2025-04-11 17:08:33 -0400

    Update skip.tsx

--- src/components/pages/skip.tsx
+++ src/components/pages/skip.tsx
@@ -89,13 +89,17 @@ export function SkipPage() {
               filterOut={{
                 source: {
                   1: ["0xFEC6a341F9B7e30E30Ef5B990158FA539B6bb057"],
-                  ...(showLedgerMainnet ? {} : { "ledger-mainnet-1": undefined }),
+                  ...(showLedgerMainnet
+                    ? {}
+                    : { "ledger-mainnet-1": undefined, 1: ["0xbf45a5029d081333407cc52a84be5ed40e181c46"] }),
                 },
                 destination: {
                   1: ["0xFEC6a341F9B7e30E30Ef5B990158FA539B6bb057"],
                   "pacific-1": ["ibc/6C00E4AA0CC7618370F81F7378638AE6C48EFF8C9203CE1C2357012B440EBDB7"],
                   "1329": ["0xB75D0B03c06A926e488e2659DF1A861F860bD3d1"],
-                  ...(showLedgerMainnet ? {} : { "ledger-mainnet-1": undefined }),
+                  ...(showLedgerMainnet
+                    ? {}
+                    : { "ledger-mainnet-1": undefined, 1: ["0xbf45a5029d081333407cc52a84be5ed40e181c46"] }),
                 },
               }}
             />
