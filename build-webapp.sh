03341b89d227dab5b179fe7e79e0d5919e5d43ef
zramsay <zach@bluecollarcoding.ca>
2025-04-17 08:55:00 -0400

    try

---
+++ build-webapp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+OUTPUT_DIR=".next"
+DEST_DIR=${1:-/data}
+
+if [[ -d "$DEST_DIR" ]]; then
+  echo "${DEST_DIR} already exists." 1>&2
+  exit 1
+fi
+
+npm install || exit 1
+npm install next@14.1.0 next-seo@6.4.0 || exit 1
+
+npm run build
+
+if [[ ! -d "$OUTPUT_DIR" ]]; then
+  echo "Missing output directory: $OUTPUT_DIR" 1>&2
+  exit 1
+fi
+
+mv "$OUTPUT_DIR" "$DEST_DIR"
+
