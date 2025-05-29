5c8c888bb5ad407849fdd451f83afa76e0369869
Nur Fikri <codingki@gmail.com>
2025-05-29 09:00:21 +0700

    remove unused

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { cleanOrigin, edgeConfigResponse, isPreview } from "./utils/api";
 
 const corsOptions = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization, solana-client, sentry-trace, baggage, x-api-key",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, solana-client, sentry-trace, baggage",
 };
 
 // Donetsk and Luhansk Regions of Ukraine, Russia, Crimea, Cuba, Iran, North Korea or Syria
