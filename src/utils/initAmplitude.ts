98ce06d09b9459567f85068b41cdc81c9b408245
Todd Kao <todd.kakao@gmail.com>
2025-08-07 11:43:24 -0400

    use proxy with amplitude on go app

--- src/utils/initAmplitude.ts
+++ src/utils/initAmplitude.ts
@@ -4,10 +4,13 @@ import packageJson from "../../package.json";
 
 let isAmplitudeInitialized = false;
 
+const serverUrl = "https://go.skip.build/api/amplitude";
+
 export const initAmplitude = () => {
   if (isAmplitudeInitialized || typeof window === "undefined") return;
   init("14616a575f32087cf0403ab8f3ea3ce0", {
     appVersion: packageJson.dependencies["@skip-go/widget"],
+    serverUrl: `${serverUrl}/httpapi`,
   });
   isAmplitudeInitialized = true;
 };
