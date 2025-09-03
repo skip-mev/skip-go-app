f9f9b8092c810b2071e531ba5fccd6a327ab3221
Nur Fikri <codingki@gmail.com>
2025-09-04 02:36:24 +0700

    add x-grpc-web

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ import { cleanOrigin, edgeConfigResponse, isPreview } from "./utils/api";
 const corsOptions = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers":
-    "Content-Type, Authorization, solana-client, sentry-trace, baggage, x-client-url, x-client-version, x-client-name, x-client-sample-rate, x-client-library",
+    "Content-Type, Authorization, solana-client, sentry-trace, baggage, x-client-url, x-client-version, x-client-name, x-client-sample-rate, x-client-library, x-grpc-web",
 };
 
 // Donetsk and Luhansk Regions of Ukraine, Russia, Crimea, Cuba, Iran, North Korea or Syria
